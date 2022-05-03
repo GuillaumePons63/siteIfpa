475911387cc1b9a9074baa30b9b8bc07cd3cec9a
Guillaumeweb <guillaume@web-63.fr>
2022-05-03 07:33:20 +0200

    départ du projet

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP!</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="cours.php"> Les cours </a>
+                <a href="cours.php"> Les cours </a>
+                <a href="cours.php"> Les cours </a>
+                <a href="cours.php"> Les cours </a>
+            </nav>
+        </header>
+
+        
+    
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'header.php'
+
+?>
+
+<p><?php echo "Hello world!"; ?></p>
+
+<?php
+
+include 'footer.php'
+
+?>
